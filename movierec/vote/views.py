77ce4b356a11b537347d0ebfaac9e277625bddc5
AE <ae@localhost.localdomain>
2024-03-02 13:16:33 -0800

    added page buttons

--- movierec/vote/views.py
+++ movierec/vote/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
 from .models import Choice, Movie
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     #list of obj
-    movie_list = Movie.objects.order_by("name")
-    type(movie_list)
-    #movie_list = get_list_or_404(Movie.objects.order_by("name"))
+   # movie_list = Movie.objects.order_by("name")
+    #type(movie_list)
+    movie_list = get_list_or_404(Movie.objects.order_by("name"))
+    paginator = Paginator(movie_list, 2)
     """    list = []
     nl = "\n"
     for m in movie_list:
         list.append(f"Movie Name: {m.name} {nl} Genre: {m.genre}")
     output = "\n ".join(list)"""
-    context = {"movie_list": movie_list}
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    #context = {"movie_list": movie_list}
+    context = {"page_obj": page_obj}
     return render(request, "vote/index.html", context)
